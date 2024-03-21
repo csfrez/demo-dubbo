2be5d1e4833c7ffb0ab750a7c02ea34cf5cfcd32
Yangzhi <yangzhi@ddjf.com.cn>
2024-03-21 11:12:20 +0800

    使用nacos为注册中心

--- demo-dubbo-consumer/src/main/java/com/csfrez/dubbo/DemoDubboConsumerApplication.java
+++ demo-dubbo-consumer/src/main/java/com/csfrez/dubbo/DemoDubboConsumerApplication.java
@@ -3,40 +3,36 @@ package com.csfrez.dubbo;
 import com.csfrez.dubbo.api.DemoService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Date;
-
 @SpringBootApplication
 @Slf4j
 public class DemoDubboConsumerApplication {
 
-	@DubboReference(version = "${demo.service.version}")
-	private DemoService demoService;
+    @DubboReference(version = "${demo.service.version}")
+    private DemoService demoService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoDubboConsumerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDubboConsumerApplication.class, args);
+    }
 
-	@Bean
-	public ApplicationRunner runner() {
-		return args -> {
-			new Thread(()-> {
-				for (int i=0; i<10; i++) {
+    @Bean
+    public ApplicationRunner runner() {
+        return args -> {
+            for (int i = 0; i < 10; i++) {
+				Thread.sleep(1000);
+				new Thread(() -> {
 					try {
-						Thread.sleep(1000);
 						log.info(" Receive result ======> " + demoService.sayHello("csfrez"));
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-						Thread.currentThread().interrupt();
+					} catch (Exception e) {
+						log.info("InterruptedException", e);
+						//Thread.currentThread().interrupt();
 					}
-				}
-			}).start();
-		};
-	}
+				}).start();
+			}
+        };
+    }
 }
--- demo-dubbo-provider/src/main/java/com/csfrez/dubbo/service/DemoServiceImpl.java
+++ demo-dubbo-provider/src/main/java/com/csfrez/dubbo/service/DemoServiceImpl.java
@@ -4,8 +4,6 @@ import com.csfrez.dubbo.api.DemoService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
-import java.util.Date;
-
 @DubboService(version = "${demo.service.version}")
 @Slf4j
 public class DemoServiceImpl implements DemoService {

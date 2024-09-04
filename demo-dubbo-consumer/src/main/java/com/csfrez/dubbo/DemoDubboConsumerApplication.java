9e92e18a85832aa95a23d67953d2c8fdc5f42c17
Yangzhi <yangzhi@ddjf.com.cn>
2024-09-04 17:53:25 +0800

    duubo升级，使用nacos为注册中心

--- demo-dubbo-consumer/src/main/java/com/csfrez/dubbo/DemoDubboConsumerApplication.java
+++ demo-dubbo-consumer/src/main/java/com/csfrez/dubbo/DemoDubboConsumerApplication.java
@@ -8,12 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @SpringBootApplication
 @Slf4j
 public class DemoDubboConsumerApplication {
 
     @DubboReference(version = "${demo.service.version}")
     private DemoService demoService;
+
+    ExecutorService executor = Executors.newFixedThreadPool(10);
 
     public static void main(String[] args) {
         SpringApplication.run(DemoDubboConsumerApplication.class, args);
@@ -22,17 +27,22 @@ public class DemoDubboConsumerApplication {
     @Bean
     public ApplicationRunner runner() {
         return args -> {
-            for (int i = 0; i < 10; i++) {
-				Thread.sleep(1000);
-				new Thread(() -> {
-					try {
-						log.info(" Receive result ======> " + demoService.sayHello("csfrez"));
-					} catch (Exception e) {
-						log.info("InterruptedException", e);
-						//Thread.currentThread().interrupt();
-					}
-				}).start();
-			}
+            //for (int i = 0; i < 10; i++) {
+            int index = 0;
+            while(true){
+                if(index%2 == 0){
+                    Thread.sleep(10000);
+                }
+                final int i = index;
+                executor.submit(() -> {
+                    try {
+                        log.info(" Receive result ======> " + demoService.sayHello("csfrez"+i));
+                    } catch (Exception e) {
+                        log.info("Exception", e);
+                    }
+                });
+                index++;
+            }
         };
     }
 }
